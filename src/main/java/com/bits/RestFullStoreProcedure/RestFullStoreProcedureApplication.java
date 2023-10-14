2eaaabd68af32071881a6043995edb168803986c
egv83 <esteban8305@hotmail.com>
2023-10-14 11:56:09 -0500

    Egv83 - Crear restFull con StoreProcedure

---
+++ src/main/java/com/bits/RestFullStoreProcedure/RestFullStoreProcedureApplication.java
@@ -0,0 +1,13 @@
+package com.bits.RestFullStoreProcedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullStoreProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullStoreProcedureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bits/RestFullStoreProcedure/RestFullStoreProcedureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.RestFullStoreProcedure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullStoreProcedureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
