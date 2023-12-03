54e453aa01fdcd42918deeed258e51edd741c51b
Esteban Vallejo <esteban8305@hotmail.com>
2023-12-02 23:47:07 -0500

    evm - Get menu

--- src/main/java/com/bits/RestFullStoreProcedure/repositories/MenuRepository.java
+++ src/main/java/com/bits/RestFullStoreProcedure/repositories/MenuRepository.java
@@ -4,9 +4,14 @@ import com.bits.RestFullStoreProcedure.entities.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     @Query("Select COUNT(m) FROM Menu m")
     Long countAllRows();
+
+    @Override
+    List<Menu> findAll();
 
 }
--- src/main/java/com/bits/RestFullStoreProcedure/services/MenuServices.java
+++ src/main/java/com/bits/RestFullStoreProcedure/services/MenuServices.java
@@ -5,6 +5,8 @@ import com.bits.RestFullStoreProcedure.repositories.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MenuServices {
 
@@ -17,5 +19,9 @@ public class MenuServices {
 
     public Menu ceateMenu(Menu menu){
         return menuRepository.save(menu);
+    }
+
+    public List<Menu> getAllMenu(){
+        return menuRepository.findAll();
     }
 }
