7a0b1e8d2dcfe26fddc5fbba0f3a783d73c4f950
Esteban Vallejo <esteban8305@hotmail.com>
2023-12-05 23:04:44 -0500

    evm - Modificacion de metodo para menu

--- src/main/java/com/bits/RestFullStoreProcedure/controllers/MenuController.java
+++ src/main/java/com/bits/RestFullStoreProcedure/controllers/MenuController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.awt.SystemColor.menu;
 
@@ -22,20 +26,41 @@ public class MenuController {
     private MenuServices menuServices;
 
     @GetMapping("/menu")
-    public ResponseEntity<List<Menu>> getTotalRows(){
+    public ResponseEntity<List<Menu>> getTotalRows() {
         List<Menu> menu1 = new ArrayList<>();
-        try{
-            for (Menu menus: menuServices.getAllMenu()){
-                if(menus.getMenu() == null) {
+        List<Menu> menuTmp = menuServices.getAllMenu();
+        try {
+            for (Menu menus : menuTmp) {
+                System.out.println("EN TOTAL ROWS");
+                System.out.println("MENU getMenu: " + menus.getMenu());
+                if (menus.getMenu() == null) {
+                    System.out.println("MENU PADRE getId: " + menus.getId());
+                    subMenu(menuTmp, menus);
                     menu1.add(menus);
                 }
             }
-           return new ResponseEntity<>(menu1,HttpStatus.OK);
-        }catch (Exception e){
+            return new ResponseEntity<>(menu1, HttpStatus.OK);
+        } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    private void subMenu(List<Menu> listMenu, Menu menuPadre) {
+        Map<Long, Menu> menuMap = listMenu.stream().collect(Collectors.toMap(Menu::getId, Function.identity()));
+        System.out.println("MENU PADRE: " + menuPadre.getId());
+        List<Menu> menuHijoList = new ArrayList<>();
+        for (Menu menuAux : listMenu) {
+            System.out.println("MENU AUX getMenu: " + menuAux.getMenu());
+            if (Objects.nonNull(menuAux.getMenu())) {
+
+                if (menuAux.getMenu().getId() == menuPadre.getId()) {
+                    System.out.println("MENU AUX getMenu.getId: " + menuAux.getMenu().getId());
+                    System.out.println("Menu HIJO: "+menuAux.getId());
+                }
+            }
+        }
     }
 
     @PostMapping("/menu")
